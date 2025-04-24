450d5af7b8fd0c4459e3bfcac3c021412864572f
Sanatan Jena <121142950+sanatanjena1@users.noreply.github.com>
2025-04-24 10:54:28 +0000

    javascript preimitive Datat Types Explain

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,18 @@
+// Primitive Data Types
+
+// Number: Represents numeric values, including integers and floating-point numbers.
+// BigInt: Represents integers of arbitrary precision, introduced in ES2020.
+// String: Represents a sequence of characters.
+// Boolean: true or false
+// Null: Represents the intentional absence of a value.
+// Undefined: Indicates that a variable has not been assigned a value.
+// Symbol: Represents a unique and immutable value, often used as object property keys (introduced in ES6).
+
+// For Example Primitive data types
+let age = 30; // Number
+let bigNumber = 9007199254740992n; // BigInt
+let name = "John Doe"; // String
+let isTrue = true; // Boolean
+let nothing = null; // Null
+let notDefined; // Undefined
+let uniqueKey = Symbol("key"); // Symbol
