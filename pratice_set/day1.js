86da23f0ac6e18eedca0dd7fef6fb4add6f3d117
Sanatan Jena <121142950+sanatanjena1@users.noreply.github.com>
2025-04-27 19:09:57 +0000

    New Pratice Set Folder Create Also Write code

---
+++ pratice_set/day1.js
@@ -0,0 +1,18 @@
+let nameOne = "SanatanOne"
+console.log(nameOne)
+
+let nameTwo = nameOne
+// console.log(nameTwo)
+
+name = "SanatanTwo"
+console.log(nameTwo)
+
+let summary = "Hello"
+console.log(`Hello My Summary ${name} Good`)
+
+
+let name2 = "World";
+console.log(`Hello, ${name2}!`); // Output: Hello, World!
+
+let my = "Good Mornig"
+console.log(`Hello Sir ${my} Have a Nice Day`)
