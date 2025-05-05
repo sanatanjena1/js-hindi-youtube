e702863fe3ead65aced063cdd7826c6c2d2fdcec
Sanatan Jena <121142950+sanatanjena1@users.noreply.github.com>
2025-05-05 15:06:27 +0000

    object arrary 1st day  pratice

---
+++ 01_basic/14_arrary.js
@@ -0,0 +1,10 @@
+const myobj = {
+    name:"Santan",
+    BloodeG:"B-v",
+    age:24,    
+}
+console.table(myobj)
+console.table(typeof(myobj))
+console.log(typeof(myobj), myobj)
+
+
---
+++ 01_basic/14_arrary2.js
@@ -0,0 +1,19 @@
+const myname = {
+    name:"Javscript",
+    age:28,
+    class:"Pg",
+};
+
+const myname1= {
+    name:"Javscript",
+    age:28,
+    class:"Pg"
+}
+const myname2={
+    name:"Javscript",
+    age:28,
+    class:"Pg",
+}
+
+
+console.log(myname,myname)
